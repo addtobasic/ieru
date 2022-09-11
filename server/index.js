1e1643ff6797b6afa671dbca9f89f840c4971970
addtobasic <addtobasic@gmail.com>
2022-09-12 00:11:18 +0900

    :sparkles: 通信の完成

--- server/index.js
+++ server/index.js
@@ -4,13 +4,23 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000"],
+  }
+});
 
 const PORT = 5000;
 
 // クライアントと通信
 io.on("connection", (socket) => {
   console.log("クライアントと接続しました");
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+
+    io.emit("received_message", data);
+  })
 
   socket.on("disconnect", () => {
     console.log("クライアントと接続が切れました");
