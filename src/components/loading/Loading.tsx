d05543731441343197dd1a24c70b1bf9959e55c7
addtobasic <addtobasic@gmail.com>
2022-09-16 04:02:49 +0900

    :pencil: ロゴ画像の非表示

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,5 +1,5 @@
 import { NoSsr } from "@material-ui/core";
-import Image from "next/image";
+// import Image from "next/image";
 import ClipLoader from "react-spinners/ClipLoader";
 import styled from "styled-components";
 
@@ -7,15 +7,15 @@ const Loading = () => (
   <NoSsr>
     <StyledWrapper>
       <StyledContainer>
-        {/* <StyledImageContainer>
-          <Image
+        <StyledImageContainer>
+          {/* <Image
             src="/images/logo.png"
             height={100}
             width={100}
             objectFit="contain"
             alt="logo"
-          />
-        </StyledImageContainer> */}
+          /> */}
+        </StyledImageContainer>
 
         <ClipLoader color="purple" size={60} />
       </StyledContainer>
--- src/modules/user/login/Login.tsx
+++ src/modules/user/login/Login.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import styled from "styled-components";
 import LoginSignInButton from "./LoginSignInButton";
 
