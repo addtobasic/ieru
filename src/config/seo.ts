c7b5bcee3c86ba642216ded83263f3bee0ec6036
addtobasic <addtobasic@gmail.com>
2022-10-25 20:33:10 +0900

    :pencil: fix: refs #21 各ファイルのプロダクト名の修正

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -3,18 +3,18 @@ import { DefaultSeoProps } from "next-seo";
 export const baseUrl = "https://slack-clone-martstech.vercel.app";
 
 export const defaultSEO: DefaultSeoProps = {
-  title: "ieta",
+  title: "ieru",
   description:
-    "Slack is a new way to communicate with your team. It’s faster, better organized, and more secure than email.",
+    "ieru is a chat tool that facilitates communication and lowers the hurdles to speaking up, while ensuring psychological safety.",
   openGraph: {
     type: "website",
     locale: "en_US",
     url: baseUrl,
-    site_name: "Slack Clone",
+    site_name: "ieru",
     images: [
       {
         url: `${baseUrl}/meta/og-image.png`,
-        alt: "Slack Clone",
+        alt: "ieru",
       },
     ],
   },
