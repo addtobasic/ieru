11ec6b4b90f31216c191238b47c9afc7ab043bf8
addtobasic <addtobasic@gmail.com>
2022-11-02 15:49:04 +0900

    :thumbsup: feat: refs #25 SidebarHeaderのcomponentへの分割

--- src/components/sidebar/header/SidebarHeader.tsx
+++ src/components/sidebar/header/SidebarHeader.tsx
@@ -1,30 +1,33 @@
-import CreateIcon from "@material-ui/icons/Create";
-import styled from "styled-components";
-import SidebarHeaderInfo from "./SidebarHeaderInfo";
+import React from "react";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { styled, useTheme } from "@mui/material/styles";
+// import SidebarHeaderInfo from "./SidebarHeaderInfo";
 
-const SidebarHeader = () => (
-  <StyledContainer>
-    <SidebarHeaderInfo />
-    <CreateIcon />
-  </StyledContainer>
-);
+interface SideBarHeaderProps {
+  handleDrawerClose: () => void;
+}
+
+const SidebarHeader: React.FC<SideBarHeaderProps> = ({ handleDrawerClose }) => {
+  const theme = useTheme();
+
+  return (
+    <DrawerHeader>
+      <IconButton onClick={handleDrawerClose}>
+        {theme.direction === "ltr" ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+      </IconButton>
+    </DrawerHeader>
+  );
+};
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
 
 export default SidebarHeader;
-
-const StyledContainer = styled.div`
-  display: flex;
-  border-bottom: 1px solid var(--sidebar-color);
-  padding: 0.75rem;
-
-  > svg {
-    background-color: white;
-    color: var(--sidebar-color);
-    border-radius: 50%;
-    height: 2rem;
-    width: 2rem;
-    font-size: 1.125rem;
-    padding: 0.4rem;
-    margin-top: auto;
-    margin-bottom: auto;
-  }
-`;
