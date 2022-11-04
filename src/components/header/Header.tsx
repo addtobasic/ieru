fe96c18e39c0cd479b9a3bdb6244e5142ab8f7af
addtobasic <addtobasic@gmail.com>
2022-11-04 20:46:45 +0900

    :pencil: fix: refs #34 styledのimport先を修正

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import { styled } from "@mui/material/styles";
 import HeaderInfo from "./info/HeaderInfo";
 import HeaderOptions from "./options/HeaderOptions";
 import HeaderSearch from "./search/HeaderSearch";
