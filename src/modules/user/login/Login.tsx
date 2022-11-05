b32f3d9b273f7031bf5e5005afc608ff08cd76fa
addtobasic <addtobasic@gmail.com>
2022-11-05 14:09:32 +0900

    :pencil: fix: refs #34 loginコンポーネントのスタイルをMUI v5に書き換え

--- src/modules/user/login/Login.tsx
+++ src/modules/user/login/Login.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styled from "styled-components";
+import { styled } from "@mui/material/styles";
 import LoginSignInButton from "./LoginSignInButton";
 
 const Login = () => (
@@ -18,21 +18,21 @@ const Login = () => (
   </StyledWrapper>
 );
 
+const StyledWrapper = styled("div")({
+  backgroundColor: "#f8f8f8",
+  height: "100vh",
+  display: "grid",
+  placeItems: "center",
+});
+
+const StyledContainer = styled("div")({
+  padding: "6.25rem",
+  textAlign: "center",
+  backgroundColor: "white",
+  borderRadius: "0.75rem",
+  boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+});
+
+const StyledTitle = styled("h1")({});
+
 export default Login;
-
-const StyledWrapper = styled.div`
-  background: #f8f8f8;
-  height: 100vh;
-  display: grid;
-  place-items: center;
-`;
-
-const StyledContainer = styled.div`
-  padding: 6.25rem;
-  text-align: center;
-  background: white;
-  border-radius: 0.75rem;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-`;
-
-const StyledTitle = styled.h1``;
