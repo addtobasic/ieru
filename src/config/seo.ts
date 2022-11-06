2028d0d409a18c19593c0150b9a6d3f41d36e803
addtobasic <addtobasic@gmail.com>
2022-11-06 14:14:14 +0900

    :pencil: fix: baseUrlの修正

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -1,11 +1,11 @@
 import { DefaultSeoProps } from "next-seo";
 
-export const baseUrl = "https://slack-clone-martstech.vercel.app";
+export const baseUrl = "https://ieru.life";
 
 export const defaultSEO: DefaultSeoProps = {
   title: "ieru",
   description:
-    "ieru is a chat tool that facilitates communication and lowers the hurdles to speaking up, while ensuring psychological safety.",
+    "ieru is a chat tool that facilitates communication and lowers the hurdles for speaking up, while ensuring psychological safety.",
   openGraph: {
     type: "website",
     locale: "en_US",
