5797f9bcf9922821fbc582a697a7728e9b2907fd
addtobasic <addtobasic@gmail.com>
2022-11-17 20:51:48 +0900

    :shirt: feat: refs #49 stylelintの設定の修正

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  extends: 'stylelint-config-standard',
+  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
   overrides: [
     {
-      files: ['**/*.{js,jsx,ts,tsx}'],
-      customSyntax: '@stylelint/postcss-css-in-js'
-    }
-  ]
-}
+      files: ["**/*.{js,jsx,ts,tsx}"],
+      customSyntax: "@stylelint/postcss-css-in-js",
+    },
+  ],
+
+  plugins: ["stylelint-order"],
+  rules: {
+    "function-no-unknown": null,
+    "value-keyword-case": null,
+    "property-no-unknown": null,
+    "annotation-no-unknown": null,
+    "no-duplicate-selectors": null,
+  },
+};
