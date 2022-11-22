f91f62d36f281f9dd99954818337a3f3d7bacdd9
addtobasic <addtobasic@gmail.com>
2022-11-23 01:55:08 +0900

    :sparkles: feat: refs #56 アイコンのTooltipの追加

--- src/components/chat/messages/ChatMessagesItemMenu.tsx
+++ src/components/chat/messages/ChatMessagesItemMenu.tsx
@@ -6,25 +6,39 @@ import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import { styled } from "@mui/material/styles";
+import Tooltip from "@mui/material/Tooltip";
 import * as React from "react";
+
+const ICON_MENU_LIST = [
+  {
+    icon: <ThumbUpAltIcon />,
+    label: "いいね",
+  },
+  {
+    icon: <ThumbDownAltIcon />,
+    label: "よくないね",
+  },
+  {
+    icon: <VisibilityOffIcon />,
+    label: "匿名化",
+  },
+  {
+    icon: <EditIcon />,
+    label: "編集",
+  },
+  {
+    icon: <DeleteIcon />,
+    label: "削除",
+  },
+];
 
 const ChatMessagesItemMenu = () => (
   <ButtonGroup size="small" aria-label="small button group">
-    <StyledButton>
-      <ThumbUpAltIcon />
-    </StyledButton>
-    <StyledButton>
-      <ThumbDownAltIcon />
-    </StyledButton>
-    <StyledButton>
-      <VisibilityOffIcon />
-    </StyledButton>
-    <StyledButton>
-      <EditIcon />
-    </StyledButton>
-    <StyledButton>
-      <DeleteIcon />
-    </StyledButton>
+    {ICON_MENU_LIST.map((item) => (
+      <Tooltip key={item.label} title={item.label}>
+        <StyledButton>{item.icon}</StyledButton>
+      </Tooltip>
+    ))}
   </ButtonGroup>
 );
 
