98059c65a47fada462db01032245cc2532390683
addtobasic <addtobasic@gmail.com>
2022-11-23 15:03:43 +0900

    :sparkles: feat: refs #56 moreアイコンを押したときにメニューの表示

--- src/components/chat/messages/ChatMessagesItemHover.tsx
+++ src/components/chat/messages/ChatMessagesItemHover.tsx
@@ -1,39 +1,57 @@
-// import DeleteIcon from "@mui/icons-material/Delete";
-// import EditIcon from "@mui/icons-material/Edit";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import ThumbDownAltIcon from "@mui/icons-material/ThumbDownAlt";
 import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
 // import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import ButtonGroup from "@mui/material/ButtonGroup";
+import Popper from "@mui/material/Popper";
 import { styled } from "@mui/material/styles";
 import Tooltip from "@mui/material/Tooltip";
 import * as React from "react";
 
-const ChatMessagesItemHover = () => (
-  <ButtonGroup size="small" aria-label="small button group">
-    <Tooltip title="いいね" placement="top">
-      <StyledButton>
-        <ThumbUpAltIcon />
-      </StyledButton>
-    </Tooltip>
-    <Tooltip title="よくないね" placement="top">
-      <StyledButton>
-        <ThumbDownAltIcon />
-      </StyledButton>
-    </Tooltip>
-    <Tooltip title="匿名化" placement="top">
-      <StyledButton>
-        <VisibilityOffIcon />
-      </StyledButton>
-    </Tooltip>
-    <Tooltip title="その他" placement="top">
-      <StyledButton>
-        <MoreHorizIcon />
-      </StyledButton>
-    </Tooltip>
-  </ButtonGroup>
-);
+import ChatMessagesItemMenu from "./ChatMessagesItemMenu";
+
+const ChatMessagesItemHover = () => {
+  // moreアイコンを押したときにメニューを表示する
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popper" : undefined;
+
+  return (
+    <>
+      <ButtonGroup size="small" aria-label="small button group">
+        <Tooltip title="いいね" placement="top">
+          <StyledButton>
+            <ThumbUpAltIcon />
+          </StyledButton>
+        </Tooltip>
+        <Tooltip title="よくないね" placement="top">
+          <StyledButton>
+            <ThumbDownAltIcon />
+          </StyledButton>
+        </Tooltip>
+        <Tooltip title="匿名化" placement="top">
+          <StyledButton>
+            <VisibilityOffIcon />
+          </StyledButton>
+        </Tooltip>
+        <Tooltip title="その他" placement="top">
+          <StyledButton onClick={handleMenuOpen}>
+            <MoreHorizIcon />
+          </StyledButton>
+        </Tooltip>
+      </ButtonGroup>
+      <Popper id={id} open={open} anchorEl={anchorEl} placement="left-start">
+        <ChatMessagesItemMenu />
+      </Popper>
+    </>
+  );
+};
 
 export default ChatMessagesItemHover;
 
--- src/components/chat/messages/ChatMessagesItemMenu.tsx
+++ src/components/chat/messages/ChatMessagesItemMenu.tsx
@@ -1,43 +1,55 @@
-// import DeleteIcon from "@mui/icons-material/Delete";
-// import EditIcon from "@mui/icons-material/Edit";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import ThumbDownAltIcon from "@mui/icons-material/ThumbDownAlt";
 import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
 // import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-import ButtonGroup from "@mui/material/ButtonGroup";
-import { styled } from "@mui/material/styles";
-import Tooltip from "@mui/material/Tooltip";
+// import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Divider from "@mui/material/Divider";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuItem from "@mui/material/MenuItem";
+import MenuList from "@mui/material/MenuList";
+import Paper from "@mui/material/Paper";
 import * as React from "react";
 
 const ChatMessagesItemMenu = () => (
-  <ButtonGroup size="small" aria-label="small button group">
-    <Tooltip title="いいね" placement="top">
-      <StyledButton>
-        <ThumbUpAltIcon />
-      </StyledButton>
-    </Tooltip>
-    <Tooltip title="よくないね" placement="top">
-      <StyledButton>
-        <ThumbDownAltIcon />
-      </StyledButton>
-    </Tooltip>
-    <Tooltip title="匿名化" placement="top">
-      <StyledButton>
-        <VisibilityOffIcon />
-      </StyledButton>
-    </Tooltip>
-    <Tooltip title="その他" placement="top">
-      <StyledButton>
-        <MoreHorizIcon />
-      </StyledButton>
-    </Tooltip>
-  </ButtonGroup>
+  <Paper sx={{ width: 200, maxWidth: "100%" }}>
+    {/* <ClickAwayListener> */}
+    <MenuList>
+      <MenuItem>
+        <ListItemText>いいね</ListItemText>
+        <ListItemIcon>
+          <ThumbUpAltIcon fontSize="small" />
+        </ListItemIcon>
+      </MenuItem>
+      <MenuItem>
+        <ListItemText>よくないね</ListItemText>
+        <ListItemIcon>
+          <ThumbDownAltIcon fontSize="small" />
+        </ListItemIcon>
+      </MenuItem>
+      <Divider />
+      <MenuItem>
+        <ListItemText>匿名化</ListItemText>
+        <ListItemIcon>
+          <VisibilityOffIcon fontSize="small" />
+        </ListItemIcon>
+      </MenuItem>
+      <MenuItem>
+        <ListItemText>チャットの修正</ListItemText>
+        <ListItemIcon>
+          <EditIcon fontSize="small" />
+        </ListItemIcon>
+      </MenuItem>
+      <MenuItem>
+        <ListItemText>チャットの削除</ListItemText>
+        <ListItemIcon>
+          <DeleteIcon fontSize="small" />
+        </ListItemIcon>
+      </MenuItem>
+    </MenuList>
+  </Paper>
 );
 
 export default ChatMessagesItemMenu;
-
-const StyledButton = styled("div")({
-  padding: "0.2rem",
-  cursor: "pointer",
-});
