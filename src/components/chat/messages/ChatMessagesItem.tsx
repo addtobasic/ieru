e0ff4a3fe6ea198fcf0e64573d92e10a2a9a544a
addtobasic <addtobasic@gmail.com>
2022-12-08 01:24:55 +0900

    :thumbsup: fix: refs #53 useEffectのクリーンアップ関数の追加

--- src/components/chat/messages/ChatMessagesItem.tsx
+++ src/components/chat/messages/ChatMessagesItem.tsx
@@ -21,6 +21,7 @@ interface ChatMessagesItemProps {
 
 const ChatMessagesItem: React.FC<ChatMessagesItemProps> = ({ message }) => {
   const [isAnonym, setIsAnonym] = useState(message.isAnonym);
+  const [likedBy, setLikedBy] = useState(message.likedBy);
   const { photoURL, user, timestamp } = message;
   const displayImage = useStore().userStore.user?.photoURL;
 
@@ -30,19 +31,37 @@ const ChatMessagesItem: React.FC<ChatMessagesItemProps> = ({ message }) => {
   const channel = store.channelStore.selectedChannel;
   const messagesRef = doc(db, "channels", channel!.id, "messages", message.id);
 
-  // チャットごとの匿名, 顕名を検知しリアルタイム同期をする
+  // チャットのデータをリアルタイム同期する
   useEffect(() => {
-    onSnapshot(messagesRef, (doc) => {
+    const unsubscribe = onSnapshot(messagesRef, (doc) => {
       setIsAnonym(doc.data()?.isAnonym);
+      setLikedBy(doc.data()?.likedBy);
     });
+
+    return () => {
+      // Unmouting
+      unsubscribe();
+    };
   }, [messagesRef]);
 
+  // 匿名, 顕名を切り替えてfirestoreのデータを更新する関数
   const handleChangeAnonym = async () => {
     await updateDoc(messagesRef, {
       isAnonym: !isAnonym,
     });
 
     setIsAnonym(!isAnonym);
+  };
+
+  // firestoreのいいねのデータを更新する関数
+  const handleAddLike = async () => {
+    if (!likedBy.includes(displayImage || "")) {
+      await updateDoc(messagesRef, {
+        likedBy: [...likedBy, displayImage],
+      });
+
+      setLikedBy([...likedBy, displayImage || ""]);
+    }
   };
 
   // マウスオーバーでメニュ－アイコンを表示する
@@ -80,7 +99,7 @@ const ChatMessagesItem: React.FC<ChatMessagesItemProps> = ({ message }) => {
         </StyledContent>
         <StyledDiv>
           <StyledButtonDiv>
-            <IconButton size="small">
+            <IconButton size="small" onClick={handleAddLike}>
               <FavoriteBorderOutlinedIcon />
             </IconButton>
             <Typography
@@ -88,11 +107,10 @@ const ChatMessagesItem: React.FC<ChatMessagesItemProps> = ({ message }) => {
                 paddingLeft: "0.15rem",
               }}
             >
-              1
+              {likedBy?.length || 0}
             </Typography>
           </StyledButtonDiv>
         </StyledDiv>
-
         <StyledPopper>
           {isHover && (
             <ChatMessagesItemHover
